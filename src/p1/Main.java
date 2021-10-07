9c5a4dc04356b8723436c5a3faff9b23f87bdcd0
Francisco Perea Mora <francisco.perea@avanzit.com>
2021-10-07 21:42:35 +0200

    Primer commit pruebas 8

---
+++ src/p1/Main.java
@@ -0,0 +1,14 @@
+package p1;
+
+import java.time.LocalTime;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		LocalTime lt = LocalTime.now();
+		System.out.println(lt);
+
+	}
+
+}
